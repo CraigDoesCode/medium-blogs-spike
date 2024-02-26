1029d17962a39f523ccd03a1c7309cd1e68673ca
Craig Norford <craig.a.norford@gmail.com>
2024-02-26 23:12:00 +0000

    init

---
+++ README.md
@@ -0,0 +1,9 @@
+# Medium blogs spike
+
+## Scope
+
+I'm building a prortfolio website and would love to have a blog sesion. I do not want to duplicate the work of writing medium articles however I would like to be able to display the blogs inlign with the design of my portfolio site.
+
+## References
+
+Based on https://medium.com/geekculture/get-posts-from-medium-api-in-2022-using-javascript-2d1e40d0fd25
---
+++ getMediumPosts.js
@@ -0,0 +1,34 @@
+const fetch = require("node-fetch");
+
+const myHeaders = new fetch.Headers();
+myHeaders.append("Content-Type", "application/json");
+const requestOptions = {
+  method: "get",
+  headers: myHeaders,
+  redirect: "follow",
+};
+
+const url = "https://v1.nocodeapi.com/craigdoescode/medium/WGNWCuqwumKAUoxO";
+async function getMediumPosts() {
+  const data = await fetch(url, requestOptions);
+  const posts = await data.json();
+
+  return posts;
+}
+
+async function logPosts() {
+  const posts = await getMediumPosts();
+  console.log(posts);
+}
+
+async function logPostDetails() {
+  const posts = await getMediumPosts();
+  posts.forEach((post) => {
+    console.log(post.title);
+    console.log(post.link);
+    console.log(new Date(post.published));
+    console.log(post.category);
+  });
+}
+
+logPostDetails();
